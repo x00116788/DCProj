8eab45f381d7941484afac7b531c10816df9effc
x00116788 <x00116788@gmail.com>
2017-06-13 06:19:14 +0100

    use transaction service

--- api/services/TransactionService.js
+++ api/services/TransactionService.js
@@ -1,17 +1,16 @@
 'use srict';
 let http = require('http'),
     fee = 1;// 1europer transaction out
-
 module.exports = {
     
-    exchange: function(from, to){
+    exchange: function(from){
 
         let promise = new Promise( (fulfill, reject) => {
             try{
-                http.get('http://free.currencyconverterapi.com/api/v3/convert?q='+ from + '_' + to + '&compact=ultra', (chunk) =>{
+                http.get('http://free.currencyconverterapi.com/api/v3/convert?q='+ from + '_EUR&compact=ultra', (chunk) =>{
                 chunk.on('data', (returnedRate) =>{
-
-                    fulfill(JSON.parse(returnedRate));
+                    let rr = returnedRate.toString().match(/([0-9]*[.])?[0-9]+/)
+                    fulfill(JSON.parse(rr[0]));
                 })
             })
             }catch(err){reject(err, 'endpoint error')}            
@@ -19,36 +18,84 @@ module.exports = {
         return promise;
     },
 
-    topup: function(){
+    topup: function(params){
         let topupPromise = new Promise( (fulfill, reject) => {
-            try {
-                this.getOne(id).then((one) =>{
-                    if (one === '404, No Card Found'){
-                        reject(one);
+            Card.findOne(params.card_ID, (err, card) => {
+                if (err){ reject (err)}
+                else if (!card){ 
+                    reject ('invalid card')
+                }
+                else{
+                    if (params.transaction_amount <= 5){
+                        reject ('top up amount below minimum')
                     }
-                    one.balance += amount;
-                    one.save(function(err) {
+                        // console.log(card);
+                    Transaction.create(params, function (err, trans){
                         if (err){reject(err)}
-                        else fulfill('top-up sucessful')
-                    })
-                })
-                return topupPromise;
-            }catch(err) {console.log(err)}
-        })
+                        else if (!trans){ 
+                            reject ('error occured')
+                        }
+                        else{
+                            trans.transaction_ref = "Top-up " + card.id + ' ' + new Date().toString();
+                            trans.description = 'top-up balance';
+                            trans.direction = 'IN';
+                            trans.status = 'Accepted';
+                            card.balance += Number(params.transaction_amount);
+                            card.save();
+                            trans.save();
+                            // console.log(trans);
+                            fulfill (trans);
+                        }                        
+                    });                    
+                } 
+            });  
+        }) 
+        return topupPromise;            
 
     },
 
-    getOne: function(id){
-        let onePromise = new Promise((res,rej) => {
-            Card.findOne(id, (err, card) => {
-                if(card){
-                    res(card);
+    spend: function(params){
+        let spendPromise = new Promise( (fulfill, reject) => {
+        var balance;
+            Card.findOne({
+                cardNumber:params.cardNumber
+                }).exec(function (err, card){
+                if (err) {
+                    reject (err);
+                }
+                if (!card || card.cvv !=params.cvv )//|| card.expiry_date != req.allParams.expiry_date )
+                { 
+                    reject ('invalid card')
+                }
+                balance = card.balance
+                Transaction.create(params, function (err, trans){
+                   if (err){
+                       reject(err)
                     }
-                 else{
-                    rej('404, No Card Found');
-                };
-            });
-        });
-        return onePromise;
+                    else if (!trans){ 
+                        reject ('error occured')
+                    }
+                    else{
+                        trans.transaction_ref = "Debit " + card.id + ' ' + new Date().toString();
+                        trans.description = 'params.merchant';
+                        trans.direction = 'OUT';
+                        trans.status = 'Accepted';
+                        card.balance -= Number(params.transaction_amount) + fee;
+                        card.save()
+                        trans.save();
+                        console.log(trans);
+                        let success = {
+                            'reference': trans.transaction_ref,
+                            'message': 'success',
+                            'previous balance': balance,
+                            'new balance': card.balance
+                        }
+                        fulfill (success); 
+                    }
+
+                })
+            })
+        })
+        return spendPromise
     }
 }
